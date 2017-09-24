efcad7bbb1779dd6b938b9bf343639f12119c34b
MoatazGitHub <moataz.mohammady@gmail.com>
2017-09-24 11:05:15 +0200

    restyle posts navigation

--- index.php
+++ index.php
@@ -58,69 +58,96 @@
     <div class="row" id="main-wrapper">
         <div class="col-lg-12 col-md-6">
 
-	    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php if ( have_posts() ) :
+			while ( have_posts() ) : the_post(); ?>
 
                 <div class="post">
                     <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 
                     <p>
                         <i class="fa fa-list" aria-hidden="true"></i>
-					    <?php the_category(', '); ?>
-                        | <i class="icon-user"></i> <i class="fa fa-user" aria-hidden="true"></i> <?php the_author_posts_link() ?>
+						<?php the_category( ', ' ); ?>
+                        | <i class="icon-user"></i> <i class="fa fa-user"
+                                                       aria-hidden="true"></i> <?php the_author_posts_link() ?>
                         | <i class="icon-calendar"></i> <i class="fa fa-calendar" aria-hidden="true"></i>
 
-					    <?php the_date('F j, Y'); ?> at <i class="fa fa-clock-o" aria-hidden="true"></i> <?php the_time('g:i a'); ?>
+						<?php the_date( 'F j, Y' ); ?> at <i class="fa fa-clock-o"
+                                                             aria-hidden="true"></i> <?php the_time( 'g:i a' ); ?>
 
                         | <i class="icon-comment"></i> <i class="fa fa-comment" aria-hidden="true"></i>
-<!--                        <a href="--><?php //comment_link() ?><!--">  --><?php //comments_number() ?><!--</a>-->
-					    <?php
-					    //Load Different CSS classes according to Comment-condition
+                        <!--                        <a href="--><?php //comment_link() ?><!--">  -->
+						<?php //comments_number() ?><!--</a>-->
+						<?php
+						//Load Different CSS classes according to Comment-condition
 
-					    $css_class = 'zero-comments';
-					    $number    = (int) get_comments_number( get_the_ID() );
+						$css_class = 'zero-comments';
+						$number    = (int) get_comments_number( get_the_ID() );
 
-					    if ( 1 === $number ){
-						    $css_class = 'one-comment';
-					    $title = 'Comment';
-					    }
-                        elseif ( 1 < $number )
-						    $css_class = 'multiple-comments';
-                         $title = "Comments";
+						if ( 1 === $number ) {
+							$css_class = 'one-comment';
+							$title     = 'Comment';
+						} elseif ( 1 < $number ) {
+							$css_class = 'multiple-comments';
+						}
+						$title = "Comments";
 
-                        comments_popup_link('No comments yet.','One Comment','% Comments',$css_class,'Comments are off for this post' )
-                        ?>
+						comments_popup_link( 'No comments yet.', 'One Comment', '% Comments', $css_class, 'Comments are off for this post' )
+						?>
 
                     </p>
-				    <?php if ( has_post_thumbnail() ) : ?>
+					<?php if ( has_post_thumbnail() ) : ?>
                         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-						    <?php the_post_thumbnail(null,['class' => 'img-responsive img-thumbnail', 'alt' => get_the_title()]); ?>
+							<?php the_post_thumbnail( null, [
+								'class' => 'img-responsive img-thumbnail',
+								'alt'   => get_the_title()
+							] ); ?>
                         </a>
-				    <?php endif; ?>
+					<?php endif; ?>
                     <article><p>
-						    <?php the_excerpt() ?>
+							<?php the_excerpt() ?>
                         </p>
 
                     </article>
 
 
-
                 </div>
 
-	    <?php endwhile; ?>
+			<?php endwhile; ?>
         </div>
 
-            <div class="posts-navigation">
-                <div class="prev-posts"><?php previous_posts_link('<i class="fa fa-chevron-left" aria-hidden="true"></i>
- Prev'); ?></div>
+        <div class="posts-navigation">
 
-                <div class="next-posts"><?php next_posts_link('<i class="fa fa-chevron-right" aria-hidden="true"></i>
- Next'); ?></div>
+
+            <div class="prev-posts"><?php
+				if ( get_previous_posts_link() ) {
+					previous_posts_link( '<i class="fa fa-chevron-left" aria-hidden="true"></i> Prev' );
+
+				} else {
+					echo '<span><i class="fa fa-chevron-left" aria-hidden="true"></i> Prev</span>';
+				}
+				?></div>
+
+            <div class="next-posts">
+
+
+	            <?php
+
+	            if ( get_next_posts_link() ) {
+		            next_posts_link( '<i class="fa fa-chevron-right" aria-hidden="true"></i> Next' );
+
+	            } else {
+		            echo '<span><i class="fa fa-chevron-right" aria-hidden="true"></i> Next</span>';
+	            }
+                ?>
+
+
 
             </div>
 
-	    <?php endif; ?>
-    </div>
+        </div>
 
+		<?php endif; ?>
+    </div>
 
 
 <?php get_footer(); ?>
